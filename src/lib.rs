43e3f7ccad4d4b62e0e51b71869eb85ecde34ae1
Giovanni Barillari <g@baro.dev>
2024-09-07 20:26:11 +0200

    Port `emmett-crypto` package

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+#[cfg(not(any(target_os = "freebsd", target_os = "windows")))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
+#[cfg(any(target_os = "freebsd", target_os = "windows"))]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+use pyo3::prelude::*;
+use std::sync::OnceLock;
+
+mod cryptography;
+
+pub fn get_lib_version() -> &'static str {
+    static LIB_VERSION: OnceLock<String> = OnceLock::new();
+
+    LIB_VERSION.get_or_init(|| {
+        let version = env!("CARGO_PKG_VERSION");
+        version.replace("-alpha", "a").replace("-beta", "b")
+    })
+}
+
+#[pymodule]
+fn _emmett_core(_py: Python, module: &Bound<PyModule>) -> PyResult<()> {
+    module.add("__version__", get_lib_version())?;
+
+    cryptography::init_pymodule(module)?;
+    Ok(())
+}
