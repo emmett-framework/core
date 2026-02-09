9557ea23a27cbadf7774d8bca6bbe4b54fa8a3ec
Giovanni Barillari <giovanni.barillari@sentry.io>
2026-02-10 00:05:35 +0100

    Catch Request cookies parsing errors

--- emmett_core/http/wrappers/__init__.py
+++ emmett_core/http/wrappers/__init__.py
@@ -61,7 +61,10 @@ class IngressWrapper(Wrapper, metaclass=ABCMeta):
     def cookies(self) -> SimpleCookie:
         cookies: SimpleCookie = SimpleCookie()
         for cookie in self.headers.get("cookie", "").split(";"):
-            cookies.load(cookie)
+            try:
+                cookies.load(cookie)
+            except Exception:
+                continue
         return cookies
 
     @property
